0a179424f8a19e6f247c3ba2f8c5aacf4c651e1e
Miriã Fassarella <miriafassarella@gmail.com>
2023-12-29 09:01:38 -0500

    Enhanced loop

--- src/exercicesPOO/arrays/Main.java
+++ src/exercicesPOO/arrays/Main.java
@@ -6,11 +6,34 @@ public class Main {
 		/*Arrays are objects*/
 		
 		/*Declaring and instantiating an array*/
-		int[] grades = new int[5]; 
-		/*or int[] grades ={8,5,4,9,10}*/
+		/*int[] grades = new int[5];*/
+		 int[] grades ={8,5,4,9,10};
 		
 		/*Initializing with values*/
 		/*int[] grades = new int[]{8,5,4,9,10};*/
 		
+		
+		int position = 5;
+		
+		System.out.println(grades[position - 1]);
+		
+		double average = calcAverage(grades);
+		System.out.println(average);
+		
+	}
+		
+	static double calcAverage(int[] numbers) {
+		double total = 0;
+		
+		/*Enhanced loop*/
+		for (int number : numbers) {
+			total += number;
+		}
+		
+		
+		/* for(int i = 0; i < numbers.length; i++) { total += numbers[i]; }*/
+		
+		
+		return total/numbers.length;
 	}
 }
