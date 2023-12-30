9035061b13eb3391154bc13482534fdeb79e7ead
Miriã Fassarella <miriafassarella@gmail.com>
2023-12-30 16:34:56 -0500

    Class Arrays and toString method

---
+++ src/exercicesPOO/arrays/ToStringMetodo.java
@@ -0,0 +1,16 @@
+package exercicesPOO.arrays;
+
+import java.util.Arrays;
+
+public class ToStringMetodo {
+
+	public static void main(String[] args) {
+		
+		int[] grades = {4,7,5,3,2};
+		
+		String gradesInString = Arrays.toString(grades);
+		
+		System.out.println(gradesInString);
+	}
+	
+}
