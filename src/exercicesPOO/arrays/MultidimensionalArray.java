7fa3a8ed52e913800ae3578dd342cc4636aba5d8
Miriã Fassarella <miriafassarella@gmail.com>
2024-01-08 21:41:16 -0500

    Array Iteration

--- src/exercicesPOO/arrays/MultidimensionalArray.java
+++ src/exercicesPOO/arrays/MultidimensionalArray.java
@@ -1,37 +1,42 @@
 package exercicesPOO.arrays;
 
+import java.util.Arrays;
 
 public class MultidimensionalArray {
 	
 	public static void main(String[] args) {
 		
-		String[][] newArray = new String[3][3];
+		String[][] newArray = new String[3][];
 		
+		newArray[0] = new String[3];
 		newArray[0][0] = "Miria";
 		newArray[0][1] = "Adriano";
 		newArray[0][2] = "Bart";
 		
+		newArray[1] = new String[3];
 		newArray[1][0] = "Suzana";
 		newArray[1][1] = "Marcio";
 		newArray[1][2] = "Murilo";
 		
+		newArray[2] = new String[3];
 		newArray[2][0] = "Fred";
 		newArray[2][1] = "Aghata";
 		newArray[2][2] = "Theo";
 		
 		
 		
-		System.out.println(newArray[0][0]);
-		System.out.println(newArray[0][1]);
-		System.out.println(newArray[0][2]);
 		
-		System.out.println(newArray[1][0]);
-		System.out.println(newArray[1][1]);
-		System.out.println(newArray[1][2]);
+		 
+		for(String[] names : newArray) {
+			for(String name : names) {
+				System.out.println(name); 
+			}
+		}
 		
-		System.out.println(newArray[2][0]);
-		System.out.println(newArray[2][1]);
-		System.out.println(newArray[2][2]);
+		
+		
+		 
+		 
 	}
 
 }
